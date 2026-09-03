f853366be60bbd441d56f5e312a5d334f166528a
tuttuwucha <tuttuwucha@gmail.com>
2026-09-03 19:38:57 +1000

    Add files via upload

---
+++ 1я-пара.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+            cat.type = "Cat";
+            cat.age = 5;
+            cat.name = "kot";
+            cat.sound = "mew";
+            Console.WriteLine(cat.getInfo());
+            Console.WriteLine(cat.sayHello()); 
+            Console.In.ReadLine();
+
+
+        }
+    }
+
+
+    public class Animal
+    {
+        public string type;
+        public int age;
+        public string name;
+        public string sound;
+
+        public string getInfo()
+        {
+            return $"Вид: {type}\nВозраст {age}\nКличка {name}";
+        }
+
+
+        public string sayHello()
+        {
+            return sound;
+        }
+    }
+}
