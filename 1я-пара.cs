68fc0263caa16e486f0df7922144a92a8685ad55
tuttuwucha <tuttuwucha@gmail.com>
2026-09-10 19:45:57 +1000

    Add files via upload

--- 1я-пара.cs
+++ 1я-пара.cs
@@ -10,13 +10,10 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            Animal cat = new Animal();
-            cat.type = "Cat";
-            cat.age = 5;
-            cat.name = "kot";
-            cat.sound = "mew";
+            Animal cat = new Animal("catr", 10, "namer");
+            
             Console.WriteLine(cat.getInfo());
-            Console.WriteLine(cat.sayHello()); 
+            
             Console.In.ReadLine();
 
 
@@ -26,10 +23,88 @@ namespace ConsoleApp8
 
     public class Animal
     {
-        public string type;
-        public int age;
-        public string name;
-        public string sound;
+        private string type;
+        private int age;
+        private string name;
+        private string sound;
+
+        public Animal(string Type, int Age, string Name)
+        {
+            type = Type;
+            age = Age;
+            name = Name;
+        }
+        public Animal()
+        {
+
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set 
+            {
+                if(value == null || value == "")
+                {
+                    Console.WriteLine("You have to enter a type");
+                }
+                else
+                {
+                    type = value;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value == null || value >= 100)
+                {
+                    Console.WriteLine("You have entered invalid value");
+
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == null || value == "")
+                {
+                    Console.WriteLine("You have to enter a name");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value == null || value == "")
+                {
+                    Console.WriteLine("You have to enter a sound");
+
+                }
+                else
+                {
+                    sound = value;
+                }
+            }
+        }
+
 
         public string getInfo()
         {
